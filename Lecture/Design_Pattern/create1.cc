8920e35e894a6839e46ceef64c1c1352c1a6d6c2
nerdooit <nerdooit@gmail.com>
2020-03-01 22:15:04 +0900

    .

---
+++ Lecture/Design_Pattern/create1.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class shape
+{
+public:
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+};
+
+class Rect : public Shape
+{
+public:
+};
+
+int main()
+{
+}
+
+
+
