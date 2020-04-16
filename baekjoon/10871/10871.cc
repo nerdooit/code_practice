0fa7ca01bb25d2382ca95a47242ae8ccbaf395d2
nerdooit <nerdooit@gmail.com>
2020-04-16 10:50:46 +0900

    4.16 baekjoon (practice1)

--- baekjoon/10871/10871.cc
+++ baekjoon/10871/10871.cc
@@ -1,10 +1,26 @@
+/*
+ * Name : nerdooit
+ * Date : 2020.4.16
+ * Description : 10871, 세수
+ */
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	int a , b;
-	cin >> a >> b;
+	vector<int> v;
+
+	for (int i = 0; i < 3; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+
+	sort(begin(v),end(v));
+
+	cout << v[1];
 }
