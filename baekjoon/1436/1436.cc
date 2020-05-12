0f999df16ffce6e7daa5ffb7bec1cab1af9f6086
nerdooit <nerdooit@gmail.com>
2020-05-12 23:32:24 +0900

    5.12 baekjoon(brute force)

--- baekjoon/1436/1436.cc
+++ baekjoon/1436/1436.cc
@@ -5,7 +5,19 @@ using namespace std;
 
 int main()
 {
-	vector<int> v;
+	int N, cnt = 0, i=666, res;
+	cin >> N;
 
-	
+	while(1) {
+		string str = to_string(i);
+		if(str.find("666") != string::npos) {
+			cnt++;
+		}
+		if (N == cnt) {
+			res = i;
+			break;
+		}
+		i++;
+	}
+	cout << i << endl;
 }
