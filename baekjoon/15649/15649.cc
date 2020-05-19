54e63946fceb6232496b7a618bd481b50b285dc3
nerdooit <nerdooit@gmail.com>
2020-05-19 23:42:17 +0900

    5.19 baekjoon(back trace)

---
+++ baekjoon/15649/15649.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int N, M, arr[9];
+
+void dfs (int cnt) {
+	for (int i = 1; i <= N; i++) {
+		arr[]
+	}
+}
+
+
+
+int main()
+{
+	stack<int> s;
+
+	cin >> N >> M;
+
+
+}
