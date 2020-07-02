6ac4eb96ba3f30beb5be038b80c71ca8cff91852
nerdooit <nerdooit@gmail.com>
2020-07-02 20:44:24 +0900

    12865

--- baekjoon/12865/12865.cc
+++ baekjoon/12865/12865.cc
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-int arr[1001][1001] = {0,};
 
 int main()
 {
@@ -10,17 +9,19 @@ int main()
 	int tmp_max = 0;
 
 	cin >> N >> W;
+	int arr[N][W+1];
+	for (int i = 0; i <= N; i++) for (int j = 0; j <= W; j++) arr[i][j] = 0;
 
 	while (N--) {
 		int tmp, tmp_w;
 		cin >> tmp >> tmp_w;
 		for (int i = 1; i <= W; i++) {
 
-			if (tmp > i) {
+			if (W > i) {
 				arr[idx][i] = arr[idx - 1][i];
 			} else {
 				if (tmp_w + arr[idx - 1][W-i] > arr[idx][i-1]) {
-					arr[idx][i] = tmp_w + arr[idx -1][W-i];
+					arr[idx][i] = tmp_w + arr[idx - 1][W-i];
 				} else {
 					arr[idx][i] = arr[idx][i-1];
 				}
@@ -29,7 +30,8 @@ int main()
 
 		if (tmp_max < arr[idx][W]) tmp_max = arr[idx][W];
 
-		idx++;
+		if (!idx) idx = 1;
+		else idx = 0;
 	}
 
 	cout << tmp_max << endl;
